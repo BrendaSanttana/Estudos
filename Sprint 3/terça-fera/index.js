fd2ccd612a07c075216a59a79cadd64c8c75df98
Brenda Santana <brenda.nsantana@outlook.com>
2023-02-27 22:20:07 -0300

    Atividades

---
+++ Sprint 3/terça-fera/index.js
@@ -0,0 +1,25 @@
+console.log("Estou no HTML, arquivo de script interno")
+console.log ("Salve")
+
+
+let MeuNumero = 10;
+let NumeroAleatorio = 25;
+
+console.log (MeuNumero + NumeroAleatorio)
+
+
+var soma = 1 + 10 + 9
+soma = 25
+soma = 20
+soma = 5 * (2 - 9)
+
+console.log (soma)
+
+let variavel1
+let variavel2
+
+console.log(variavel1 + variavel2)
+
+let cpf1 = 036056011-99
+let cpf2 = "036056011-99"
+console.log (cpf1)
