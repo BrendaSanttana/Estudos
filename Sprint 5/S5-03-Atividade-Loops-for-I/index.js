6bbd7ec7afdc9fe8e69c09e69d5c56f98a8f32e9
Brenda Santana <brenda.nsantana@outlook.com>
2023-02-28 14:37:46 -0300

    atividades

---
+++ Sprint 5/S5-03-Atividade-Loops-for-I/index.js
@@ -0,0 +1,31 @@
+
+//function renderUpToTwenty() {
+// for(let i = 0; i <21; i++) {
+/// console.log (i); // OK feito
+//}
+//}
+//renderUpToTwenty()
+
+//function tenInTenToAHundred () {
+//for(let i = 0; i <110; i +=10){
+//console.log (i); // OK feito
+//}
+//}
+
+
+function tenInTenToAHundred() {
+    for (let i = 0; i < 20; i++) {
+        if (i % 1 !== 0)
+            console.log(i)
+    }
+}
+tenInTenToAHundred()
+
+//function oddUpToTwenty() {
+  //  for (let i = 2; i < 22; i++) {
+       // if (i % 2 === 0)
+           // console.log(i)
+   // }
+//}
+
+//oddUpToTwenty()
