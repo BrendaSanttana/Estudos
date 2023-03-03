f64049a73fb0255000191bf1d98554c6a79d3958
Brenda N. Santana <brenda.nsantana@outlook.com>
2023-03-03 11:15:47 -0300

    atividades

--- TESTES/index.js
+++ TESTES/index.js
@@ -1,8 +1,15 @@
 
-// Exibir os números de 1 à 5
-const n = 5;
-// looping de i = 1 até 5
-// em cada iteração, i é aumentado em 1
-for (let i = 1; i <= n; i++) {
-    console.log(i); // imprimindo o valor de i
+
+
+function validacao (email, senha) {
+    const emailValido = "brenda.nsantana@outlook.com"
+    const senhaValida = "brendasantana"
+
+    if (emailValido === email && senhaValida === senha) {
+     return true
+    }
+    return false
+    
 }
+
+console.log(validacao("brenda.nsantana@outlook.com", "brendasantana"))
---
+++ TESTES/index1.js
@@ -0,0 +1,23 @@
+function validacao (email, senha) {
+    const emailValido = "brenda.nsantana@outlook.com"
+    const senhaValido = "brendasantana"
+
+    if (emailValida === email && senhaValida === senha){
+        return true
+    }
+    {
+    return false
+    }
+}
+
+//console.log(validacao("brenda.nsantana@outlook.com", "brendasantana"))
+
+
+
+
+
+
+
+
+
+
